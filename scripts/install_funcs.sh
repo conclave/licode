6c20c5aaa2411f8cb18b9b7a12e963be4534f54a
zyxar <chou.marcus@gmail.com>
2015-04-23 13:23:41 +0800

    improve install_funcs.sh

--- scripts/install_funcs.sh
+++ scripts/install_funcs.sh
@@ -1,11 +1,13 @@
 install_opus(){
   local VERSION="1.1"
-  mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+  mkdir -p ${BUILD_DIR} && pushd ${BUILD_DIR}
   curl -O http://downloads.xiph.org/releases/opus/opus-${VERSION}.tar.gz
   tar xf opus-${VERSION}.tar.gz
-  cd opus-${VERSION}
+  pushd opus-${VERSION}
   ./configure --prefix=${PREFIX_DIR}
   make -s V=0 && make install
+  popd
+  popd
 }
 
 install_libav(){
@@ -14,7 +16,7 @@ install_libav(){
   local SRC="${DIR}.tar.gz"
   local URL="https://www.libav.org/releases/${SRC}"
   local MD5SUM="ce788f04693d63ae168b696eac8ca888"
-  mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+  mkdir -p ${BUILD_DIR} && pushd ${BUILD_DIR}
   [[ ! -s ${SRC} ]] && curl -O ${URL}
   if ! (echo "${MD5SUM} ${SRC}" | md5sum --check) ; then
     rm -f ${SRC} && curl -O ${URL} # re-try
@@ -22,33 +24,39 @@ install_libav(){
   fi
   rm -rf ${DIR}
   tar xf ${SRC}
-  cd ${DIR}
+  pushd ${DIR}
   [[ "${ENABLE_GPL}" == "true" ]] && \
   PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig ./configure --prefix=${PREFIX_DIR} --enable-shared --enable-libvpx --enable-libopus --enable-gpl --enable-libx264 || \
   PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig ./configure --prefix=${PREFIX_DIR} --enable-shared --enable-libvpx --enable-libopus && \
   make -j4 -s V=0 && make install
+  popd
+  popd
 }
 
 install_libnice(){
-  mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+  mkdir -p ${BUILD_DIR} && pushd ${BUILD_DIR}
   curl -O http://nice.freedesktop.org/releases/libnice-0.1.4.tar.gz
   tar xf libnice-0.1.4.tar.gz
-  cd libnice-0.1.4
+  pushd libnice-0.1.4
   patch -R ./agent/conncheck.c < ${ROOT_DIR}/etc/libnice-014.patch0
   PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig:${PREFIX_DIR}/lib64/pkgconfig:${PKG_CONFIG_PATH} ./configure --prefix=${PREFIX_DIR} && \
   make -s V= && make install
+  popd
+  popd
 }
 
 install_openssl(){
   local SSL_VERSION="1.0.2a"
-  mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+  mkdir -p ${BUILD_DIR} && pushd ${BUILD_DIR}
   curl -O http://www.openssl.org/source/openssl-${SSL_VERSION}.tar.gz
   tar xf openssl-${SSL_VERSION}.tar.gz
-  cd openssl-${SSL_VERSION}
+  pushd openssl-${SSL_VERSION}
   ./config no-ssl3 --prefix=$PREFIX_DIR -fPIC && \
   make depend && \
   make -s V=0 && \
   make install
+  popd
+  popd
 }
 
 install_libsrtp(){
@@ -64,13 +72,15 @@ install_libuv(){
   local UV_VERSION="0.10.36"
   local UV_SRC="https://github.com/libuv/libuv/archive/v${UV_VERSION}.tar.gz"
   local UV_DST="libuv-${UV_VERSION}.tar.gz"
-  mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+  mkdir -p ${BUILD_DIR} && pushd ${BUILD_DIR}
   [[ ! -s ${UV_DST} ]] && wget -c ${UV_SRC} -O ${UV_DST}
   tar xf ${UV_DST}
-  cd libuv-${UV_VERSION} && make
+  pushd libuv-${UV_VERSION} && make
   ##make_install:
   cp -av include/* ${PREFIX_DIR}/include
   local symbol=$(readelf -d ./libuv.so | grep soname | sed 's/.*\[\(.*\)\]/\1/g')
   ln -s libuv.so ${symbol}
   cp -dv libuv.so ${symbol} ${PREFIX_DIR}/lib
+  popd
+  popd
 }
