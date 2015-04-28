81754f512282e51a9f68b508b2f5de622997f118
zyxar <chou.marcus@gmail.com>
2015-04-28 23:58:57 +0800

    erizo:: agent - now run from anywhere

--- src/erizo/agent/index.js
+++ src/erizo/agent/index.js
@@ -1,8 +1,9 @@
-/*global require, process, GLOBAL*/
+/*global require, process, __dirname, GLOBAL*/
 'use strict';
 
 var Getopt = require('node-getopt');
 var spawn = require('child_process').spawn;
+var path = require('path');
 
 // Configuration default values
 GLOBAL.config = {};
@@ -89,7 +90,7 @@ var launchErizoJS = function() {
     var fs = require('fs');
     var out = fs.openSync('./erizo-' + id + '.log', 'a');
     var err = fs.openSync('./erizo-' + id + '.log', 'a');
-    var erizoProcess = spawn('./launch.sh', ['../node-erizo', id, privateIP, publicIP], { detached: true, stdio: [ 'ignore', out, err ] });
+    var erizoProcess = spawn(path.resolve(__dirname, './launch.sh'), [path.resolve(__dirname, '../node-erizo'), id, privateIP, publicIP], { detached: true, stdio: [ 'ignore', out, err ] });
     erizoProcess.unref();
     erizoProcess.on('close', function (code) {
         var index = idle_erizos.indexOf(id);
