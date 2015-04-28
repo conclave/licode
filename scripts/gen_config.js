feb846e87860996793da3043d3d771ae637c2355
zyxar <chou.marcus@gmail.com>
2015-04-28 10:42:03 +0800

    refine d422ea1: retain log4js_configuration.json

--- scripts/gen_config.js
+++ scripts/gen_config.js
@@ -6,12 +6,13 @@ var fs = require('fs');
 var path = require('path');
 var homeDir = path.resolve(__dirname, '../local');
 var config = require('../etc/licode_default');
+config.logger = require('../etc/log4js_configuration');
 
 fs.writeFileSync(path.join(homeDir, 'etc/common.json'), JSON.stringify({
   rabbit: config.rabbit,
   cloudProvider: config.cloudProvider
 }, null, '  '));
-fs.writeFileSync(path.join(homeDir, 'etc/logger.json'), JSON.stringify(config.logger, null, '  '));
+fs.writeFileSync(path.join(homeDir, 'etc/log4js_configuration.json'), JSON.stringify(config.logger, null, '  '));
 fs.writeFileSync(path.join(homeDir, 'etc/nuve.json'), JSON.stringify(config.nuve, null, '  '));
 fs.writeFileSync(path.join(homeDir, 'etc/erizoController.json'), JSON.stringify(config.erizoController, null, '  '));
 fs.writeFileSync(path.join(homeDir, 'etc/erizoAgent.json'), JSON.stringify(config.erizoAgent, null, '  '));
--- src/common/logger.js
+++ src/common/logger.js
@@ -4,7 +4,7 @@
 var path = require('path');
 var log4js = require('log4js');
 var homeDir = path.resolve(__dirname, '../../local');
-var config = require(path.join(homeDir, 'etc/logger'));
+var config = require(path.join(homeDir, 'etc/log4js_configuration'));
 log4js.configure(config);
 module.exports = function create (name) {
   return log4js.getLogger(name);
