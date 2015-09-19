fa03bef79b71971478fd8cacc20cb7f8ff5e6f83
Markus Tzoe <chou.marcus@gmail.com>
2015-09-19 14:22:07 +0800

    add simple test.js for node-erizo

---
+++ src/erizo/node-erizo/test/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var path = require('path');
+process.chdir(path.resolve(__dirname, '../../agent/')); // resolve log4cxx.properties
+var addon = require('../addon');
+var conn = new addon.WebRtcConnection(
+  true,
+  true,
+  '',
+  0,
+  0,
+  0,
+  false,
+  '',
+  0,
+  '',
+  ''
+);
+conn.init();
+console.log(conn.getLocalSdp());
+
+var evt = new addon.CrossEvent(function(v) {
+  console.log(v);
+});
+
+setInterval(function() {
+  evt.emit(''+Math.random());
+}, 100);
+
+setTimeout(function() {
+  process.exit();
+}, 2000);
