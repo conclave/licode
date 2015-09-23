03350f0301a8583e78b423b21225288164ffa687
zyxar <chou.marcus@gmail.com>
2015-09-23 09:37:07 +0800

    liberizo: clang-format StringUtil

--- src/liberizo/StringUtil.h
+++ src/liberizo/StringUtil.h
@@ -5,15 +5,11 @@
 #include <vector>
 
 namespace erizo {
-
-    namespace stringutil {
-
-    std::vector<std::string> splitOneOf(const std::string& str,
-                                        const std::string& delims,
-                                        const size_t maxSplits = 0);
-
-    }
-
+namespace stringutil {
+  std::vector<std::string> splitOneOf(const std::string& str,
+      const std::string& delims,
+      const size_t maxSplits = 0);
+}
 }
 
 #endif // STRINGUTIL_H_
