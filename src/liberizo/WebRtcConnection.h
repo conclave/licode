269f9601d2d933fca0e55f7ab768b40a02a388d6
zyxar <chou.marcus@gmail.com>
2015-09-23 10:02:31 +0800

    liberizo: clang-format WebRtcConnection

--- src/liberizo/WebRtcConnection.h
+++ src/liberizo/WebRtcConnection.h
@@ -1,19 +1,19 @@
 #ifndef WEBRTCCONNECTION_H_
 #define WEBRTCCONNECTION_H_
 
-#include <string>
-#include <queue>
-#include <boost/thread/mutex.hpp>
-#include <boost/thread.hpp>
-
 #include "AsyncCallback.h"
 #include "logger.h"
-#include "SdpInfo.h"
 #include "MediaDefinitions.h"
-#include "Transport.h"
-#include "Stats.h"
 #include "rtp/webrtc/fec_receiver_impl.h"
 #include "rtp/RtcpProcessor.h"
+#include "SdpInfo.h"
+#include "Stats.h"
+#include "Transport.h"
+
+#include <boost/thread/mutex.hpp>
+#include <boost/thread.hpp>
+#include <queue>
+#include <string>
 
 namespace erizo {
 
@@ -39,103 +39,115 @@ enum WebRTCEvent {
  * A WebRTC Connection. This class represents a WebRTC Connection that can be established with other peers via a SDP negotiation
  * it comprises all the necessary Transport components.
  */
-class WebRtcConnection: public MediaSink, public MediaSource, public FeedbackSink, public FeedbackSource, public TransportListener, public webrtc::RtpData {
+class WebRtcConnection : public MediaSink, public MediaSource, public FeedbackSink, public FeedbackSource, public TransportListener, public webrtc::RtpData {
   DECLARE_LOGGER();
-public:
-    /**
+
+  public:
+  /**
      * Constructor.
      * Constructs an empty WebRTCConnection without any configuration.
      */
-    WebRtcConnection(bool audioEnabled, bool videoEnabled, const IceConfig& iceConfig, bool trickleEnabled, AsyncCallback* listener);
-    /**
+  WebRtcConnection(bool audioEnabled, bool videoEnabled, const IceConfig& iceConfig, bool trickleEnabled, AsyncCallback* listener);
+  /**
      * Destructor.
      */
-    virtual ~WebRtcConnection();
-    /**
+  virtual ~WebRtcConnection();
+  /**
      * Inits the WebConnection by starting ICE Candidate Gathering.
      * @return True if the candidates are gathered.
      */
-    bool init();
-    void close();
-    /**
+  bool init();
+  void close();
+  /**
      * Sets the SDP of the remote peer.
      * @param sdp The SDP.
      * @return true if the SDP was received correctly.
      */
-    bool setRemoteSdp(const std::string &sdp);
+  bool setRemoteSdp(const std::string& sdp);
 
-    /**
+  /**
      * Add new remote candidate (from remote peer).
      * @param sdp The candidate in SDP format.
      * @return true if the SDP was received correctly.
      */
-    bool addRemoteCandidate(const std::string &mid, int mLineIndex, const std::string &sdp);
-    /**
+  bool addRemoteCandidate(const std::string& mid, int mLineIndex, const std::string& sdp);
+  /**
      * Obtains the local SDP.
      * @return The SDP as a string.
      */
-    std::string getLocalSdp();
+  std::string getLocalSdp();
 
-    int deliverAudioData(char* buf, int len);
-    int deliverVideoData(char* buf, int len);
+  int deliverAudioData(char* buf, int len);
+  int deliverVideoData(char* buf, int len);
 
-    int deliverFeedback(char* buf, int len);
-  
-    // changes the outgoing payload type for in the given data packet
-    void changeDeliverPayloadType(dataPacket *dp, packetType type);
-    // parses incoming payload type, replaces occurence in buf
-    void parseIncomingPayloadType(char *buf, int len, packetType type);
+  int deliverFeedback(char* buf, int len);
 
-    /**
+  // changes the outgoing payload type for in the given data packet
+  void changeDeliverPayloadType(dataPacket* dp, packetType type);
+  // parses incoming payload type, replaces occurence in buf
+  void parseIncomingPayloadType(char* buf, int len, packetType type);
+
+  /**
      * Sends a PLI Packet 
      * @return the size of the data sent
      */
-    int sendPLI();  
+  int sendPLI();
 
-    /**
+  /**
      * Gets the current state of the Ice Connection
      * @return
      */
-    WebRTCEvent getCurrentState();
-    
-    std::string getJSONStats();
+  WebRTCEvent getCurrentState();
 
-    void onTransportData(char* buf, int len, Transport *transport);
+  std::string getJSONStats();
 
-    void updateState(TransportState state, Transport * transport);
+  void onTransportData(char* buf, int len, Transport* transport);
 
-    void queueData(int comp, const char* data, int len, Transport *transport, packetType type);
+  void updateState(TransportState state, Transport* transport);
 
-    void onCandidate(const CandidateInfo& cand, Transport *transport);
+  void queueData(int comp, const char* data, int len, Transport* transport, packetType type);
 
-    void setFeedbackReports(bool shouldSendFb, uint32_t rateControl=0){
-      this->shouldSendFeedback_ = shouldSendFb;
-      this->rateControl_ = rateControl;
-    };
+  void onCandidate(const CandidateInfo& cand, Transport* transport);
 
+  void setFeedbackReports(bool shouldSendFb, uint32_t rateControl = 0)
+  {
+    this->shouldSendFeedback_ = shouldSendFb;
+    this->rateControl_ = rateControl;
+  };
 
-    // webrtc::RtpHeader overrides.
-    int32_t OnReceivedPayloadData(const uint8_t* payloadData, const uint16_t payloadSize,const webrtc::WebRtcRTPHeader* rtpHeader);
-    bool OnRecoveredPacket(const uint8_t* packet, int packet_length);
+  // webrtc::RtpHeader overrides.
+  int32_t OnReceivedPayloadData(const uint8_t* payloadData, const uint16_t payloadSize, const webrtc::WebRtcRTPHeader* rtpHeader);
+  bool OnRecoveredPacket(const uint8_t* packet, int packet_length);
 
-private:
-  static const int STATS_INTERVAL = 5000;
-  
+  private:
+  static const int STATS_INTERVAL{ 5000 };
+
   SdpInfo remoteSdp_;
   SdpInfo localSdp_;
-
   boost::shared_ptr<RtcpProcessor> rtcpProcessor_;
-
   Stats thisStats_;
-
   WebRTCEvent globalState_;
-
   int bundle_, sequenceNumberFIR_;
   boost::mutex receiveVideoMutex_, updateStateMutex_, feedbackMutex_;
   boost::thread send_Thread_;
   std::queue<dataPacket> sendQueue_;
   AsyncCallback* connEventListener_;
-  Transport *videoTransport_, *audioTransport_;
+  Transport* videoTransport_, *audioTransport_;
+
+  bool audioEnabled_;
+  bool videoEnabled_;
+  bool trickleEnabled_;
+  bool shouldSendFeedback_;
+  uint32_t rateControl_; //Target bitrate for hacky rate control in BPS
+
+  IceConfig iceConfig_;
+  int stunPort_, minPort_, maxPort_;
+  std::string stunServer_;
+
+  webrtc::FecReceiverImpl fec_receiver_;
+  boost::condition_variable cond_;
+
+  struct timeval now_, mark_;
 
   bool sending_;
   void sendLoop();
@@ -145,22 +157,6 @@ private:
   int deliverFeedback_(char* buf, int len);
   std::string getJSONCandidate(const std::string& mid, const std::string& sdp);
   void notifyEvent(WebRTCEvent newEvent, const std::string& message);
-  
-  bool audioEnabled_;
-  bool videoEnabled_;
-  bool trickleEnabled_;
-  bool shouldSendFeedback_;
-  uint32_t rateControl_; //Target bitrate for hacky rate control in BPS 
-  
-  IceConfig iceConfig_;
-  int stunPort_, minPort_, maxPort_;
-  std::string stunServer_;
-
-  webrtc::FecReceiverImpl fec_receiver_;
-  boost::condition_variable cond_;
-
-
-  struct timeval now_, mark_;
 };
 
 } /* namespace erizo */
